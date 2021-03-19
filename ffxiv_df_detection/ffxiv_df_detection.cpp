a35fbd040085ffffbb3e3b391ab05e6ecb83d33d
SageCreations <sagecreations.ec@gmail.com>
2021-03-19 14:50:54 -0500

    working window capture

---
+++ ffxiv_df_detection/ffxiv_df_detection.cpp
@@ -0,0 +1,99 @@
+// ffxiv_df_detection.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "opencv2/calib3d.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+
+#include <iostream> 
+#include <vector>
+#include <Windows.h>
+
+cv::Mat getMat(HWND hWND) {
+
+	HDC deviceContext = GetDC(hWND);
+	HDC memoryDeviceContext = CreateCompatibleDC(deviceContext);
+
+	RECT windowRect;
+	GetClientRect(hWND, &windowRect);
+
+	int height = windowRect.bottom;
+	int width = windowRect.right;
+
+	HBITMAP bitmap = CreateCompatibleBitmap(deviceContext, width, height);
+
+	SelectObject(memoryDeviceContext, bitmap);
+
+	//copy data into bitmap
+	BitBlt(memoryDeviceContext, 0, 0, width, height, deviceContext, 0, 0, SRCCOPY);
+
+
+	//specify format by using bitmapinfoheader!
+	BITMAPINFOHEADER bi;
+	bi.biSize = sizeof(BITMAPINFOHEADER);
+	bi.biWidth = width;
+	bi.biHeight = -height;
+	bi.biPlanes = 1;
+	bi.biBitCount = 32;
+	bi.biCompression = BI_RGB;
+	bi.biSizeImage = 0; //because no compression
+	bi.biXPelsPerMeter = 1; //we
+	bi.biYPelsPerMeter = 2; //we
+	bi.biClrUsed = 3; //we ^^
+	bi.biClrImportant = 4; //still we
+
+	cv::Mat mat = cv::Mat(height, width, CV_8UC4); // 8 bit unsigned ints 4 Channels -> RGBA
+
+	//transform data and store into mat.data
+	GetDIBits(memoryDeviceContext, bitmap, 0, height, mat.data, (BITMAPINFO*)&bi, DIB_RGB_COLORS);
+
+	//clean up!
+	DeleteObject(bitmap);
+	DeleteDC(memoryDeviceContext); //delete not release!
+	ReleaseDC(hWND, deviceContext);
+
+	return mat;
+}
+
+int main(int argc, char** argv)
+{
+	LPCWSTR window_title = L"FINAL FANTASY XIV";
+	HWND hWND = FindWindow(NULL, window_title);
+	cv::namedWindow("output", cv::WINDOW_NORMAL);
+	int key = 0;
+
+	while (true) {
+		cv::Mat target = getMat(hWND);
+
+		cv::Mat background;
+		target.copyTo(background);
+		
+		cv::cvtColor(target, target, cv::COLOR_BGR2HSV); // Convert the image into HSV image
+		
+		cv::rectangle(target, cv::Point(0, 0), cv::Point(640, 30), CV_RGB(0, 0, 0), cv::FILLED); //set top menue black
+
+		
+		
+		cv::imshow("output", background);
+		
+		key = cv::waitKey(30);
+	}
+	
+    return 0;
+}
+
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
